fa5358269dec61bf98510f74111a2523d1e16de8
immanuel4082 <immanuel4082@gmail.com>
2024-10-13 19:21:14 +0300

    message

--- anhf-portal/src/app/news/news.component.ts
+++ anhf-portal/src/app/news/news.component.ts
@@ -5,144 +5,130 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <div class="hero mt-20">
-      <div class="hero-background">
-        <img src="../../assets/heron.jpg" alt="Hero background" />
-      </div>
-      <div class="hero-content">
-        <h1>News</h1>
-        <!-- <p>
-          At Alfayo Nelson Hope Foundation, our impactful programs have been
-          well received by the community, with many embracing the opportunities
-          we offer for growth and support. Our initiatives aim to create
-          positive change and uplift those in need, fostering a spirit of unity
-          and resilience. We are excited to share updates on our recent
-          activities and achievements as we continue to work towards a brighter
-          future for our Community. Stay informed about our latest developments
-          and stories of hope by exploring the links below.
-        </p> -->
-      </div>
-    </div>
-    <section id="news">
-      <img src="../../assets/logo.png" alt="Watermark Logo" class="watermark" />
-      <div class="news-content"></div>
-      <div class="video-gallery">
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/eXxH4hyL7ZM/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/eXxH4hyL7ZM?si=x9--skAYaUkDqxiN"></a>
-          <div class="video-description">
-            Alfayo at Nyali daily show podcast
+    <div class=" mt-20">
+      <div class="max-w-6xl mx-auto py-10">
+        <div class="py-10  rounded-xl bg-white text-black font-medium px-10 ">
+          <p class="md:text-5xl text-center text-3xl font-light">
+            News and Articles
+          </p>
+
+          <p class="md:text-2xl text-xl text-center font-light">
+            At Alfayo Nelson Hope Foundation, our impactful programs have been
+            well received by the community, with many embracing the
+            opportunities we offer for growth and support. Our initiatives aim
+            to create positive change and uplift those in need, fostering a
+            spirit of unity and resilience. We are excited to share updates on
+            our recent activities and achievements as we continue to work
+            towards a brighter future for our Community. Stay informed about our
+            latest developments and stories of hope by exploring the links
+            below.
+          </p>
+          <div class="flex flex-col pt-3 gap-10 ">
+            <div class="col-span-1 md:grid grid-cols-3 gap-10">
+              <a
+                href="https://youtu.be/eXxH4hyL7ZM?si=x9--skAYaUkDqxiN"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/eXxH4hyL7ZM/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo at Nyali daily show podcast
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/V2nv269tm5A?si=VW2dt8gscJUxVAMa"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/V2nv269tm5A/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo at Sauti ya Pwani Fm
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/vcKouzu7FkQ?si=f1QXMR0jHyN5VR3y"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/vcKouzu7FkQ/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo at Freretown Event
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/KnQTKqXeP8c?si=B-Y8Hc_19hCHVnPh"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/KnQTKqXeP8c/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo at St Elizabeth School
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/JuPvfHhtpKs?si=FjLK-D6aLGIkR9r6"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/JuPvfHhtpKs/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  I will stand with the Community
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/xEgKbvkLPDs?si=aoaOIM-UFQpzpJz0"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/xEgKbvkLPDs/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Champion Of The people
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/xEgKbvkLPDs?si=aoaOIM-UFQpzpJz0"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/xEgKbvkLPDs/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  My community is my pride
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/YWHVfzvvRFo?si=ON1uENFD0Xk4rVb6"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/YWHVfzvvRFo/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo at Msenangu fm
+                </p>
+              </a>
+              <a
+                href="https://youtu.be/O2R1f6Gr-ww?si=IMq0behe_cXvm0O7"
+                class="col-span-1 rounded-xl border flex flex-col justify-center"
+                ><img
+                  src="https://img.youtube.com/vi/O2R1f6Gr-ww/hqdefault.jpg"
+                  class="col-span-1 rounded-xl border flex flex-col justify-center"
+                />
+                <p class="text-2xl font-light break-words p-4">
+                  Alfayo in radio
+                </p>
+              </a>
+            </div>
           </div>
         </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/V2nv269tm5A/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/V2nv269tm5A?si=VW2dt8gscJUxVAMa"></a>
-          <div class="video-description">Alfayo at Sauti ya Pwani Fm</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/vcKouzu7FkQ/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/vcKouzu7FkQ?si=f1QXMR0jHyN5VR3y"></a>
-          <div class="video-description">Alfayo at Freretown Event</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/KnQTKqXeP8c/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/KnQTKqXeP8c?si=B-Y8Hc_19hCHVnPh"></a>
-          <div class="video-description">Alfayo at St Elizabeth School</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/JuPvfHhtpKs/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/JuPvfHhtpKs?si=FjLK-D6aLGIkR9r6"></a>
-          <div class="video-description">I will stand with the Community</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/xEgKbvkLPDs/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/xEgKbvkLPDs?si=aoaOIM-UFQpzpJz0"></a>
-          <div class="video-description">Champion Of The people</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/xEgKbvkLPDs/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/xEgKbvkLPDs?si=aoaOIM-UFQpzpJz0"></a>
-          <div class="video-description">My community is my pride</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/NzEHaiRL6AY/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/NzEHaiRL6AY?si=cGyGupPuOGPtxWUa"></a>
-          <div class="video-description">Alfayo na Boda boda</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/qSzrWslyo0Q/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/qSzrWslyo0Q?si=o5LSWWjoA6QoKXR2"></a>
-          <div class="video-description">Karibu Kongowea Alfayo</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/DmHNrI8c-Cg/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/DmHNrI8c-Cg?si=-15__DbEDsD51ji-"></a>
-          <div class="video-description">My Journey</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/4R1oeiyIk7E/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/4R1oeiyIk7E?si=PTJS-CHOSYmEM0rE"></a>
-          <div class="video-description">Alfayo na Vijana</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/YWHVfzvvRFo/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/YWHVfzvvRFo?si=ON1uENFD0Xk4rVb6"></a>
-          <div class="video-description">Alfayo at Msenangu fm</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/MW2pjmeZjos/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/MW2pjmeZjos?si=9Jk4YkVOlX70uB4-"></a>
-          <div class="video-description">Alfayo Soccer Beach</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/7ZGAY3g7UuQ/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/7ZGAY3g7UuQ?si=MefXr7K3AmdCQpZd"></a>
-          <div class="video-description">Meeting ACCs & Chiefs</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/CjTgEI71p3s/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/CjTgEI71p3s?si=_jmgSjv4vPsjpr2d"></a>
-          <div class="video-description">Alfayo at Viena Ziwa la Ng'ombe</div>
-        </div>
-        <div
-          class="video-box"
-          style="background-image: url('https://img.youtube.com/vi/O2R1f6Gr-ww/hqdefault.jpg');"
-        >
-          <a href="https://youtu.be/O2R1f6Gr-ww?si=IMq0behe_cXvm0O7"></a>
-          <div class="video-description">Alfayo at Msenangu fm</div>
-        </div>
       </div>
-    </section>
+    </div>
   `,
   styleUrl: './news.component.css',
 })
